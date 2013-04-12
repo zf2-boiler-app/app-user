bb36f92de4652b4e946a5b44397f5a52aab90d58
neilime <escalle.emilien@gmail.com>
2013-04-12 19:15:15 +0200

    Clean project

--- tests/BoilerAppUserTest/ModuleTest.php
+++ tests/BoilerAppUserTest/ModuleTest.php
@@ -1,19 +1,3 @@
 <?php
 namespace BoilerAppUserTest;
-class ModuleTest extends \PHPUnit_Framework_TestCase{
-	/**
-	 * @var \BoilerAppDisplay\Module
-	 */
-	protected $module;
-
-	/**
-	 * @see PHPUnit_Framework_TestCase::setUp()
-	 */
-	protected function setUp(){
-		$this->module = new \BoilerAppUser\Module();
-	}
-
-    public function testGetConfig(){
-        $this->assertTrue(is_array($this->module->getConfig()));
-    }
-}
+class ModuleTest extends \BoilerAppTest\PHPUnit\TestCase\AbstractModuleTestCase{}
