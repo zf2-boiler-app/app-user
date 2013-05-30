c9c5ee085742ce321dba8b1da5011ae5e405787b
neilime <escalle.emilien@gmail.com>
2013-05-30 18:45:51 +0200

    Clean test configuration

--- tests/configuration.php
+++ tests/configuration.php
@@ -3,33 +3,13 @@ return array(
 	'paths' => array(
     	'avatarsPath' => __DIR__.'/_files/avatars'
     ),
-	'translator' => array(
-		'locale' => 'fr_FR'
-	),
-	'asset_bundle' => array(
-		'cachePath' => __DIR__.'/_files/cache',
-		'cacheUrl' => '@zfBaseUrl/cache/',
-		'assetsPath' => null
-	),
 	'doctrine' => array(
 		'connection' => array(
 			'orm_default' => array(
-				'driverClass' => 'Doctrine\DBAL\Driver\PDOMySql\Driver',
 				'params' => array(
-					'host'     => 'localhost',
-					'user'     => 'root',
-					'password' => '',
-					'dbname'   => 'app-user-tests'
+					'dbname' => 'app-user-tests'
 				)
 			)
-		)
-	),
-	'service_manager' => array(
-		'factories' => array(
-			'Logger' => function(){
-				$oLogger = new \Zend\Log\Logger();
-				return $oLogger->addWriter(new \Zend\Log\Writer\Stream(STDERR));
-			}
 		)
 	)
 );
