013082757d22b58bad302962206bba00c3efe3c6
neilime <escalle.emilien@gmail.com>
2013-08-20 09:18:17 +0200

    Adapt assets config for AssetsBundle 2.1

--- config/module.config.assets.php
+++ config/module.config.assets.php
@@ -1,11 +1,13 @@
 <?php
 return array(
     'assets' => array(
-    	'BoilerAppUser\Controller\UserAccount' => array(
-    		'js' => array(
-    			__DIR__ . '/../assets/js/Validator/user-display-name.js',
-    			__DIR__ . '/../assets/js/Controller/UserAccountIndexController.js'
-    		)
+    	'BoilerAppUser' => array(
+	    	'BoilerAppUser\Controller\UserAccount' => array(
+	    		'js' => array(
+	    			__DIR__ . '/../assets/js/Validator/user-display-name.js',
+	    			__DIR__ . '/../assets/js/Controller/UserAccountIndexController.js'
+	    		)
+	    	)
     	)
     )
 );
